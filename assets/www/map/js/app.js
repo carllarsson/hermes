7677273e8b1f1fad45eb3a4160dfe214912cc105
Joakim Lundin <joakim.lundin@su.se>
2012-12-12 13:18:22 +0100

    Added comments.

--- assets/www/map/js/app.js
+++ assets/www/map/js/app.js
@@ -4,6 +4,7 @@ require.config({
     // Require plugins
     async:'../../js/lib/requirejs-plugins/async',
     text:'../../js/lib/requirejs-plugins/text',
+
     // Dependencies
     jquery:'../../js/lib/jquery-1.8.2.min',
     jquery_mobile:'../../js/lib/jquery.mobile-1.2.0.min',
@@ -11,6 +12,7 @@ require.config({
     underscore:'../../js/lib/underscore-1.3.3-min',
     backbone:'../../js/lib/backbone-0.9.2-min',
     i18n:'../../js/lib/i18next-1.5.8.min',
+
     // Application
     map:'../'
   },
