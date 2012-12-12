fc8154186ecf43ada9cb7c4bc29c70eecf8b32cb
Joakim Lundin <joakim.lundin@su.se>
2012-12-12 08:00:39 +0100

    Removed commented code.

--- assets/www/js/su-app-global.js
+++ assets/www/js/su-app-global.js
@@ -1,35 +1,20 @@
-  $( document ).bind( "mobileinit", function() {
-    // Make your jQuery Mobile framework configuration changes here!
-    $.support.cors = true;
-    $.mobile.allowCrossDomainPages = true;
-    
-    $.mobile.pushStateEnabled=false;
-    $.mobile.page.prototype.options.domCache = true;
-  });
- 
-  
-  
-  
-  $.mobile.loadPage( 'sukatsearch/index.html', { showLoadMsg: true } );
+$(document).bind("mobileinit", function () {
+  // Make your jQuery Mobile framework configuration changes here!
+  $.support.cors = true;
+  $.mobile.allowCrossDomainPages = true;
 
-  
-  function openChildBrowser(url) {
-    window.plugins.childBrowser.showWebPage(url, { showLocationBar: true });
-    /*
-     try {
-     var childBrowser;
-     childBrowser = new ChildBrowser();
-     childBrowser.showWebPage(url);
+  $.mobile.pushStateEnabled = false;
+  $.mobile.page.prototype.options.domCache = true;
+});
 
-     }
-     catch (err)
-     {
-     alert(err);
-     }
-     */
-  }
 
-  window.plugins.childBrowser.onClose = function () {
-    window.plugins.childBrowser.close();
-  };
-  
+$.mobile.loadPage('sukatsearch/index.html', { showLoadMsg:true });
+
+
+function openChildBrowser(url) {
+  window.plugins.childBrowser.showWebPage(url, { showLocationBar:true });
+}
+
+window.plugins.childBrowser.onClose = function () {
+  window.plugins.childBrowser.close();
+};
