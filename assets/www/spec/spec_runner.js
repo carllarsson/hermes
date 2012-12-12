dd2f324d2f347b46a46666bef8eb126ed8ca81cf
Joakim Lundin <joakim.lundin@su.se>
2012-12-12 09:59:38 +0100

    Added comments.

--- assets/www/spec/spec_runner.js
+++ assets/www/spec/spec_runner.js
@@ -2,20 +2,27 @@ require.config({
   baseUrl:"map/js",
   //urlArgs:'cb=' + Math.random(),
   paths:{
+    // Require plugins
     async:'../../js/lib/requirejs-plugins/async',
     text:'../../js/lib/requirejs-plugins/text',
+
+    // Dependencies
     jquery:'../../js/lib/jquery-1.8.2.min',
     jquery_mobile:'../../js/lib/jquery.mobile-1.2.0.min',
     jquery_mobile_config:'jquery.mobile-config',
     underscore:'../../js/lib/underscore-1.3.3-min',
     backbone:'../../js/lib/backbone-0.9.2-min',
     i18n:'../../js/lib/i18next-1.5.8.min',
+
+    // Test framework
     jasmine:'../../spec/lib/jasmine-1.2.0/jasmine',
     'jasmine-html':'../../spec/lib/jasmine-1.2.0/jasmine-html',
     sinon:'../../spec/lib/sinon-1.5.2',
     helper:'../../spec/helper',
     console_runner:'../../spec/lib/phantom-jasmine/console-runner',
     jasmine_junit_reporter:'../../spec/lib/jasmine-reporters/jasmine.junit_reporter',
+
+    // Tests & stuff
     spec:'../../spec',
     fixtures:'../../spec/fixtures',
     // Paths to non modules, TODO: convert to AMD
