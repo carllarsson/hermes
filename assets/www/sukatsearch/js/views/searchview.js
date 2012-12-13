e39dbeb946606881b811d6d1558838e95a56d23b
Joakim Lundin <joakim.lundin@su.se>
2012-12-13 07:36:48 +0100

    Converted sukat search module to require.js

--- assets/www/sukatsearch/js/views/searchview.js
+++ assets/www/sukatsearch/js/views/searchview.js
@@ -1,55 +1,65 @@
-var SukatSearchView = Backbone.View.extend({
+define([
+  'underscore',
+  'backbone',
+  'sukat/js/views/personview',
+  'sukat/js/models/personmodel',
+  'jquery_mobile'
+], function (_, Backbone, PersonView, Person) {
+  var SukatSearchView = Backbone.View.extend({
 
-  initialize: function () {
-    _.bindAll(this, "render", "doSearch", "resetSearchResults");
+    initialize:function () {
+      _.bindAll(this, "render", "doSearch", "resetSearchResults");
 
-    this.collection = new Persons();
-    this.collection.on("reset", this.resetSearchResults, this);
-  },
+      this.collection = new Person.Collection();
+      this.collection.on("reset", this.resetSearchResults, this);
+    },
 
-  render: function () {
-    var template = _.template($("#search_template").html(), {});
-    this.el.innerHTML = template;
-  },
+    render:function () {
+      var template = _.template($("#search_template").html(), {});
+      this.el.innerHTML = template;
+    },
 
-  events: {
-    "click a[id=search_button]": "doSearch"
-  },
+    events:{
+      "click a[id=search_button]":"doSearch"
+    },
 
-  doSearch: function (event) {
-    this.collection.fetch({
-      data: {user: $("#search_input").val().trim()},
-      error: function () {
-        alert("ERROR! Failed to fetch search results.");
-      }
-    });
-  },
+    doSearch:function (event) {
+      this.collection.fetch({
+        data:{user:$("#search_input").val().trim()},
+        error:function () {
+          alert("ERROR! Failed to fetch search results.");
+        }
+      });
+    },
 
-  resetSearchResults: function () {
-    var variables = { result_count: this.collection.length };
-    var template = _.template($("#result_template").html(), variables);
-    this.$el.children('#result_content').html(template);
+    resetSearchResults:function () {
+      var variables = { result_count:this.collection.length };
+      var template = _.template($("#result_template").html(), variables);
+      this.$el.children('#result_content').html(template);
 
-    var that = this;
-    _.each(this.collection.models, function (item) {
-      that.renderPerson(item);
-    });
+      var that = this;
+      _.each(this.collection.models, function (item) {
+        that.renderPerson(item);
+      });
 
-    $('#search_page').trigger("pagecreate");
-    $('#search_page').listview('refresh');
-  },
+      $('#search_page').trigger("pagecreate");
+      $('#search_page').listview('refresh');
+    },
 
-  renderPerson: function (item) {
-    var personView = new PersonView({
-      model: item
-    });
+    renderPerson:function (item) {
+      var personView = new PersonView({
+        model:item
+      });
 
-    var $item = personView.render().el;
-    $item.jqmData('itemId', item.get('id'));
-    $item.bind('click', function () {
-      $('#details_page').jqmData('itemId', $(this).jqmData('itemId'));
-    });
+      var $item = personView.render().el;
+      $item.jqmData('itemId', item.get('id'));
+      $item.bind('click', function () {
+        $('#details_page').jqmData('itemId', $(this).jqmData('itemId'));
+      });
 
-    this.$el.find('#result_list').append($item);
-  }
+      this.$el.find('#result_list').append($item);
+    }
+  });
+
+  return SukatSearchView;
 });
